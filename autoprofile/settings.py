0fb629d2ee38822202d49130ea11ff9b189acf76
Davud Haji <davudhaji@192.168.0.131>
2022-11-03 11:31:20 +0400

    delete somethink

--- autoprofile/settings.py
+++ autoprofile/settings.py
@@ -98,11 +98,11 @@ WSGI_APPLICATION = 'autoprofile.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_DB', 'autoprofile'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '3WfeXuUJk6ldEbmh'),
-        'HOST': os.environ.get('POSTGRES_HOST', '23.88.99.94'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'NAME': os.environ.get('POSTGRES_DB', ''),
+        'USER': os.environ.get('POSTGRES_USER', ''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_PORT', ''),
     },
 }
 
